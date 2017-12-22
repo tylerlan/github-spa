9d750327632752ae97a693a20cff08f461177549
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-12-22 07:04:07 -0800

    Add redux dev tools to store

--- src/store.js
+++ src/store.js
@@ -1,19 +1,28 @@
 /* eslint-disable max-len, no-unused-vars */
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-
+import logger from 'redux-logger';
 import rootReducer from './reducers/index';
 import GITHUB_API from './utils/Api';
 
-
- /* eslint-disable no-underscore-dangle */
+// With Dev Tools
+/* eslint-disable no-underscore-dangle */
 const store = createStore(
   rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunkMiddleware.withExtraArgument({ GITHUB_API })),
-
+  logger,
 );
+
+// Without Dev Tools
+/* eslint-disable */
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunkMiddleware.withExtraArgument({ GITHUB_API })),
+// );
+
 store.subscribe(() => {
   console.log('SUBSCRIBER store changed', store.getState());
 });
- /* eslint-enable */
+/* eslint-enable */
 export default store;
